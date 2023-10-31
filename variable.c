7fb19bcb781998656ef604260c7c267b8fc2d82b
Tanvir Ahammed Sourov <124513528+Tanvir088@users.noreply.github.com>
2023-10-31 23:46:56 +0600

    About variable in C

---
+++ variable.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+   int main (){
+
+    //variables are case snsitive.
+    //Can be use letter or Underscore(_) for starting variable name.
+    //Can not use blank space in variable name.
+ 
+ int Number;
+ int age;
+ int year;                // Here Number,year,age,month and day are integer type variable.
+ int month;
+ int day;          
+
+
+
+    return 0;
+   }
